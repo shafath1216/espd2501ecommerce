ec9c3aacd033daa1f429904ece2d1bfdc1cdd022
Shafath Mahmud choudhury~ <shafath061@gmail.com>
2025-07-31 13:06:40 +0600

    added changes

--- ecom/settings.py
+++ ecom/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return render(request,'main.html')
+
+def cart(request):
+  return render(request,'home/cart.html')
+
+def checkout(request):
+  return render(request,'checkout.html')
